15ecf6d6019d914fa0f300a78a2ffb6522ba1a74
= <=>
2019-06-10 13:11:45 -0700

    bootstrap added

--- public/js/script.js
+++ public/js/script.js
@@ -3,25 +3,21 @@ function calcMacros(){
     var output = document.getElementById("macros"); //Where the results will go
     var explanation = document.getElementById("explanation"); //Where the extended results will go
     var weight = document.getElementById("bodyWeight").value; //Bodyweight input data entered
-    var kgChecked = document.getElementById("kg").checked;  //returns checked state of Kilograms
+    var select = document.getElementById("selector");
+    const metricOption = select.options[select.selectedIndex].value;
+
 
     output.style.display = "none";        //Hiding results in case values entered are implausible
     explanation.style.display = "none";   
    
     if(calories === "" || weight === ""){   //Making sure fields are not empty
         alert("Please Enter Values");   
-    } else if (calories >= 15000){    
-        alert("Calories must be less than or equal to 15000");  
-    } else if (weight >= 3000){
-        alert("Weight must be less than or equal to 1400");
-    } else if (calories <= weight){
-        alert("Calories must not be less than or equal to weight");
     } else {
         output.style.display = "block";        
         explanation.style.display = "block";  //Displaying results if requirements are met
     }
 
-    weight = (kgChecked) ? weight *= 2.205 : weight; //formula if Kilograms is checked 
+    weight = (metricOption === 'kg') ? weight *= 2.205 : weight; //formula if Kilograms is checked 
 
     var fatCal = calories * .18;                 //26 - 34 Formula for Macros
     gramsOfFat = Math.round(fatCal / 9);         
